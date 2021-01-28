117a216d29b710e6dbb6d7f249732d441827dbd3
tony0721 <antoinequesnel11@gmail.com>
2021-01-28 16:41:55 -0500

    Three functional control points, with a little bug

--- client/src/app/components/resize-container/resize-container.component.ts
+++ client/src/app/components/resize-container/resize-container.component.ts
@@ -2,8 +2,9 @@ import { AfterViewInit, Component, ElementRef, HostListener, Input, OnInit, View
 
 const enum Status {
     OFF = 0,
-    RESIZE = 1,
-    MOVE = 2,
+    RESIZE_DIAGONAL = 1,
+    RESIZE_HORIZONTAL = 2,
+    RESIZE_VERTICAL = 3,
 }
 
 @Component({
@@ -12,17 +13,17 @@ const enum Status {
     styleUrls: ['./resize-container.component.scss'],
 })
 export class ResizeContainerComponent implements OnInit, AfterViewInit {
-    @Input('width') public width: number;
-    @Input('height') public height: number;
-    @Input('left') public left: number;
-    @Input('top') public top: number;
+    @Input() width: number;
+    @Input() height: number;
+    @Input() left: number;
+    @Input() top: number;
 
-    @ViewChild('box') public box: ElementRef;
+    @ViewChild('box') box: ElementRef;
     private boxPosition: { left: number; top: number };
     private containerPos: { left: number; top: number; right: number; bottom: number };
-    public mouse: { x: number; y: number };
-    public status: Status = Status.OFF;
-    private mouseClick: { x: number; y: number; left: number; top: number };
+    mouse: { x: number; y: number };
+    status: Status = Status.OFF;
+    // private mouseClick: { x: number; y: number; left: number; top: number };
 
     constructor() {
         // Avoid linting error
@@ -31,67 +32,57 @@ export class ResizeContainerComponent implements OnInit, AfterViewInit {
     ngOnInit(): void {
         // Avoid linting error
     }
-    ngAfterViewInit() {
+    ngAfterViewInit(): void {
         this.loadBox();
         this.loadContainer();
     }
 
-    private loadBox() {
+    private loadBox(): void {
         const { left, top } = this.box.nativeElement.getBoundingClientRect();
         this.boxPosition = { left, top };
     }
 
-    private loadContainer() {
+    private loadContainer(): void {
         const left = this.boxPosition.left - this.left;
         const top = this.boxPosition.top - this.top;
-        const right = left + 600;
-        const bottom = top + 450;
+        const right = left + 1500;
+        const bottom = top + 1500;
         this.containerPos = { left, top, right, bottom };
     }
 
-    setStatus(event: MouseEvent, status: number) {
+    setStatus(event: MouseEvent, status: number): void {
         if (status === 1) event.stopPropagation();
-        else if (status === 2) this.mouseClick = { x: event.clientX, y: event.clientY, left: this.left, top: this.top };
         else this.loadBox();
         this.status = status;
     }
 
     @HostListener('window:mousemove', ['$event'])
-    onMouseMove(event: MouseEvent) {
+    onMouseMove(event: MouseEvent): void {
         this.mouse = { x: event.clientX, y: event.clientY };
-
-        if (this.status === Status.RESIZE) this.resize();
-        else if (this.status === Status.MOVE) this.move();
+        this.resize();
     }
 
-    private resize() {
+    @HostListener('window:mouseUp', ['$event'])
+    onMouseUp(event: MouseEvent): void {
+        this.onMouseDownContainer(event);
+    }
+
+    onMouseDownContainer(event: MouseEvent): void {
+        this.setStatus(event, Status.OFF);
+    }
+
+    private resize(): void {
         if (this.resizeCondMeet()) {
-            this.width = Number(this.mouse.x > this.boxPosition.left) ? this.mouse.x - this.boxPosition.left : 0;
-            this.height = Number(this.mouse.y > this.boxPosition.top) ? this.mouse.y - this.boxPosition.top : 0;
+            if (this.status === Status.RESIZE_DIAGONAL || this.status === Status.RESIZE_HORIZONTAL) {
+                this.width = Number(this.mouse.x > this.boxPosition.left) ? this.mouse.x - this.boxPosition.left : 0;
+            }
+            if (this.status === Status.RESIZE_DIAGONAL || this.status === Status.RESIZE_VERTICAL) {
+                this.height = Number(this.mouse.y > this.boxPosition.top) ? this.mouse.y - this.boxPosition.top : 0;
+            }
         }
     }
 
-    private resizeCondMeet() {
+    private resizeCondMeet(): boolean {
         return this.mouse.x < this.containerPos.right && this.mouse.y < this.containerPos.bottom;
-    }
-
-    private move() {
-        if (this.moveCondMeet()) {
-            this.left = this.mouseClick.left + (this.mouse.x - this.mouseClick.x);
-            this.top = this.mouseClick.top + (this.mouse.y - this.mouseClick.y);
-        }
-    }
-
-    private moveCondMeet() {
-        const offsetLeft = this.mouseClick.x - this.boxPosition.left;
-        const offsetRight = this.width - offsetLeft;
-        const offsetTop = this.mouseClick.y - this.boxPosition.top;
-        const offsetBottom = this.height - offsetTop;
-        return (
-            this.mouse.x > this.containerPos.left + offsetLeft &&
-            this.mouse.x < this.containerPos.right - offsetRight &&
-            this.mouse.y > this.containerPos.top + offsetTop &&
-            this.mouse.y < this.containerPos.bottom - offsetBottom
-        );
     }
 }
