15e7329c019ddf1e2b59d6fa0ec13fa22004e6dc
Henri <firebacon23@gmail.com>
2021-02-05 18:00:14 -0500

    Bug fixes + architecture + thickness selector

--- client/src/app/classes/drawing-tool.ts
+++ client/src/app/classes/drawing-tool.ts
@@ -1,6 +1,8 @@
+import { ColorService } from '@app/services/color/color.service';
+import { DrawingService } from '@app/services/drawing/drawing.service';
 import { Tool } from './tool';
 export class DrawingTool extends Tool {
-    public thickness: number;
+    thickness: number;
     constructor(protected drawingService: DrawingService, protected colorService: ColorService, toolName: string) {
         super(drawingService, toolName);
     }
