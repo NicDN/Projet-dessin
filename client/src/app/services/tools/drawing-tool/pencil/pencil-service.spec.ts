d63430ac3b9a6f3f8cbdf5b593757a0668b8af5a
Henri <firebacon23@gmail.com>
2021-02-11 03:34:37 -0500

    Refactoring ellipse + bug fix

--- client/src/app/services/tools/drawing-tool/pencil/pencil-service.spec.ts
+++ client/src/app/services/tools/drawing-tool/pencil/pencil-service.spec.ts
@@ -111,7 +111,6 @@ describe('PencilService', () => {
         expect(imageData.data[0]).toEqual(0); // R
         expect(imageData.data[1]).toEqual(0); // G
         expect(imageData.data[2]).toEqual(0); // B
-        // tslint:disable-next-line:no-magic-numbers
         expect(imageData.data[3]).not.toEqual(0); // A
     });
 });
