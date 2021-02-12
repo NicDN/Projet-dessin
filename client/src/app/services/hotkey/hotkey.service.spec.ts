df092bbe32346c5782c9874e6e4b3bcf469b9319
tony0721 <antoinequesnel11@gmail.com>
2021-02-12 10:14:35 -0500

    More precise test for hotkey service

--- client/src/app/services/hotkey/hotkey.service.spec.ts
+++ client/src/app/services/hotkey/hotkey.service.spec.ts
@@ -1,20 +1,23 @@
-import { async, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { DrawingComponent } from '@app/components/drawing/drawing.component';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 import { HotkeyService } from './hotkey.service';
+// import SpyObj = jasmine.SpyObj;
 
-describe('HotkeyService', () => {
+fdescribe('HotkeyService', () => {
     let service: HotkeyService;
-    // let drawServiceSpy: jasmine.SpyObj<DrawingService>;
+    let fixture: ComponentFixture<DrawingComponent>;
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
+            declarations: [DrawingComponent],
             imports: [RouterTestingModule],
             providers: [DrawingService],
         }).compileComponents();
     }));
 
     beforeEach(() => {
-        // drawServiceSpy = jasmine.createSpyObj('DrawingService', ['handleNewDrawing']);
         service = TestBed.inject(HotkeyService);
     });
 
@@ -24,13 +27,24 @@ describe('HotkeyService', () => {
 
     it('ctrl functions should only be called when the ctrl key is pressed down', () => {
         const drawingServiceSpy: jasmine.Spy<any> = spyOn<any>(service.drawingService, 'handleNewDrawing');
-
-        const noCtrlKeyEvent = new KeyboardEvent('keydown', { code: 'KeyO', ctrlKey: false });
-        service.onKeyDown(noCtrlKeyEvent);
-        expect(drawingServiceSpy).not.toHaveBeenCalled();
-
         const ctrlKeyEvent = new KeyboardEvent('keydown', { code: 'KeyO', ctrlKey: true });
         service.onKeyDown(ctrlKeyEvent);
         expect(drawingServiceSpy).toHaveBeenCalled();
+    });
+
+    it('should not call ctrl functions when the ctrl key is not pressed down', () => {
+        const drawingServiceSpy: jasmine.Spy<any> = spyOn<any>(service.drawingService, 'handleNewDrawing');
+        const noCtrlKeyEvent = new KeyboardEvent('keydown', { code: 'KeyO', ctrlKey: false });
+        service.onKeyDown(noCtrlKeyEvent);
+        expect(drawingServiceSpy).not.toHaveBeenCalled();
+    });
+
+    it('ctrl key should prevent default and then should put it back to true', () => {
+        fixture = TestBed.createComponent(DrawingComponent);
+        fixture.detectChanges();
+        const event = jasmine.createSpyObj('KeyboardEvent', ['preventDefault'], { code: 'KeyO', ctrlKey: true });
+        service.onKeyDown(event);
+        expect(event.preventDefault).toHaveBeenCalled();
+        expect(event.returnValue).toEqual(true);
     });
 });
