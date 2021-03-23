9c8c5d40e14e666775f68a8b06db866c4c625438
Henri <firebacon23@gmail.com>
2021-03-23 18:59:22 -0400

    Started to fix a problem with export tests

--- client/src/app/services/option/export/export.service.spec.ts
+++ client/src/app/services/option/export/export.service.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { MatDialog } from '@angular/material/dialog';
-
 import { ExportService } from './export.service';
 
 // tslint:disable: no-string-literal
@@ -24,6 +23,7 @@ describe('ExportService', () => {
     });
 
     it('#exportCanvas should export canvas to jpeg if jpeg format is provided by the user', () => {
+        spyOn(service['exportLink'], 'click');
         spyOn(service.canvasToExport, 'toBlob');
         service.exportCanvas(FILE_NAME, JPEG_FILE_FORMAT);
         expect(service['exportLink'].getAttribute('download')).toBe(FILE_NAME + '.' + JPEG_FILE_FORMAT);
@@ -32,6 +32,7 @@ describe('ExportService', () => {
     });
 
     it('#exportCanvas should export canvas to png if png format is provided by the user', () => {
+        spyOn(service['exportLink'], 'click');
         spyOn(service.canvasToExport, 'toBlob');
         service.exportCanvas(FILE_NAME, PNG_FILE_FORMAT);
         expect(service['exportLink'].getAttribute('download')).toBe(FILE_NAME + '.' + PNG_FILE_FORMAT);
@@ -40,6 +41,7 @@ describe('ExportService', () => {
     });
 
     it('#exportCanvas should set the file name to the default name if a file name is not provided', () => {
+        spyOn(service['exportLink'], 'click');
         const DEFAULT_FILE_NAME = 'Sans titre';
         const NOT_PROVIDED_FILE_NAME = '';
         service.exportCanvas(NOT_PROVIDED_FILE_NAME, PNG_FILE_FORMAT);
