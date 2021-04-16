07164304147f7865cf66e3a02de60328e3b73722
grouleau <gabriel.rouleau@polymtl.ca>
2021-04-16 09:29:26 -0400

    Merge with master

--- client/src/app/services/tools/text/textService/text.service.spec.ts
+++ client/src/app/services/tools/text/textService/text.service.spec.ts
@@ -1,16 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ColorService } from '@app/services/color/color.service';
+import { DrawingService } from '@app/services/drawing/drawing.service';
+import { UndoRedoService } from '@app/services/undo-redo/undo-redo.service';
 import { TextService } from './text.service';
 
-describe('TextService', () => {
+fdescribe('TextService', () => {
     let service: TextService;
+    let drawingServiceSpyObj: jasmine.SpyObj<DrawingService>;
+    let undoRedoServiceSpyObj: jasmine.SpyObj<UndoRedoService>;
+    let colorServiceSpyObj: jasmine.SpyObj<ColorService>;
 
     beforeEach(() => {
+        drawingServiceSpyObj = jasmine.createSpyObj('DrawingService', ['clearCanvas']);
+        undoRedoServiceSpyObj = jasmine.createSpyObj('UndoRedoService', ['']);
+        colorServiceSpyObj = jasmine.createSpyObj('ColorService', ['']);
         TestBed.configureTestingModule({
             providers: [
                 { provide: MatBottomSheet, useValue: {} },
                 { provide: MatSnackBar, useValue: {} },
+                { provide: DrawingService, useValue: drawingServiceSpyObj },
+                { provide: ColorService, useValue: colorServiceSpyObj },
+                { provide: UndoRedoService, useValue: undoRedoServiceSpyObj },
             ],
         });
         service = TestBed.inject(TextService);
@@ -19,4 +31,6 @@ describe('TextService', () => {
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
+
+    // it('#onMouseDown should ')
 });
